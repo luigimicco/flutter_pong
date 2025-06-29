6e71251d24cacc39a0796dd9c42a68e4b0c7167b
Luigi Micco <luigi.micco@gmail.com>
2025-06-29 18:31:11 +0200

    add first draft for pong fle

---
+++ lib/pong.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'globals.dart';
+
+class PongGame extends StatefulWidget {
+  const PongGame({super.key});
+
+  @override
+  State<PongGame> createState() => _PongGameState();
+}
+
+class _PongGameState extends State<PongGame> with TickerProviderStateMixin {
+  late AnimationController _gameController;
+
+  Offset ballVelocity = const Offset(0, 0);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _gameController = AnimationController(
+      duration: Duration(milliseconds: (1 / 60).toInt()),
+      vsync: this,
+    )..addListener(_updateGame);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _initializeGame();
+      });
+      _startGameLoop();
+    });
+  }
+
+  @override
+  void dispose() {
+    _gameController.dispose();
+    super.dispose();
+  }
+
+  void _startGameLoop() {
+    _gameController.repeat();
+  }
+
+  void _initializeGame() {
+    GAME_SIZE = const Size(0, 0);
+    ballVelocity = const Offset(0, 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: GestureDetector(
+          onTapDown: (details) {},
+          onPanUpdate: (details) {},
+          child: Stack(
+            children: [
+              // Game canvas
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
