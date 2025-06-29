cf1eb4ee4cc085f5e235446a33af481397b4074c
Luigi Micco <luigi.micco@gmail.com>
2025-06-29 18:39:27 +0200

    new globals const

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 
 // global variables
 Size GAME_SIZE = const Size(0, 0);
+
+// global consts
+const double BALL_RADIUS = 8.0;
+const double BALL_SPEED = 350.0;
+
+const double PADDLE_WIDTH = 80.0;
+const double PADDLE_HEIGHT = 12.0;
+
+const Color BALL_PAINT = Colors.white;
+const Color PADDLE_PAINT = Colors.white;
+const Color LINE_PAINT = Colors.grey;
+const double LINE_WIDTH = 4;
